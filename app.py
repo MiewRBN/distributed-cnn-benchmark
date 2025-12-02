3f56c6fb48c5b28f47d5bebe5a358960489d53a5
Azmi Nur Shidiq Ridwan <150212979+MiewRBN@users.noreply.github.com>
2025-12-02 18:37:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -631,7 +631,7 @@ st.markdown("<br><br>", unsafe_allow_html=True)
 st.markdown("""
     <div class="footer">
         <p><b>UAS Komputasi Paralel dan Terdistribusi</b></p>
-        <p>Dibuat dengan ❤️ menggunakan Streamlit & TensorFlow</p>
-        <p style='font-size: 0.8rem; color: #999;'>© 2024 | Parallel Deep Learning System</p>
+        <p>Dibuat oleh abi dan teman teman menggunakan Streamlit & TensorFlow</p>
+        <p style='font-size: 0.8rem; color: #999;'>© 2025 | Parallel Deep Learning System</p>
     </div>
 """, unsafe_allow_html=True)
